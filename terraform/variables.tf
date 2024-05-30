7c63cba20c663dfedc8601c4e3ec968049bc17bb
Marco Ramirez <fliaramiaram@gmail.com>
2024-05-30 16:00:40 -0400

    First upload

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+  default     = "East US"
+}
